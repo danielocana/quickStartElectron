b7124f3a0c9fbf2e50df84df296549a631c36ab3
Daniel ocaña <daniel.ocana@scmspain.com>
2016-04-16 16:42:02 +0200

    Initial commit.

---
+++ electron-quick-start-darwin-x64/electron-quick-start.app/Contents/Resources/app/js/code.js
@@ -0,0 +1,56 @@
+var THREE = require('three');
+
+var scene, camera, renderer;
+var geometry, material, mesh;
+
+webgl();
+animate();
+
+function webgl() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.z = 1000;
+  geometry = new THREE.BoxGeometry(200, 200, 200);
+  material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+  renderer.render(scene, camera);
+}
+
+function init()
+{
+  if(navigator.webkitGetUserMedia)
+  {
+    navigator.webkitGetUserMedia({video:true}, onSuccess, onFail);
+  }
+  else
+  {
+    alert('webRTC not available');
+  }
+}
+
+function onSuccess(stream)
+{
+  document.getElementById('camFeed').src = webkitURL.createObjectURL(stream);
+}
+
+function onFail()
+{
+  alert('could not connect stream');
+}
+
+function takePhoto()
+{
+  var c = document.getElementById('photo');
+  var v = document.getElementById('camFeed');
+  c.getContext('2d').drawImage(v, 0, 0, 320, 240);
+}
